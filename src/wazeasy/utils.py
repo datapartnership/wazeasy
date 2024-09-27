dbccd088707499dbda5178eeab361e00974e8d09
msoltadeo <37631263+msoltadeo@users.noreply.github.com>
2024-09-27 19:25:28 -0400

    add example on how to use the library (#3)
    Co-authored-by: SahitiSarva <55742994+SahitiSarva@users.noreply.github.com>

--- src/wazeasy/utils.py
+++ src/wazeasy/utils.py
@@ -6,6 +6,26 @@ import dask.dataframe as dd
 import itertools
 from shapely import wkt
 
+def load_data_parquet(main_path, year, month, storage_options):
+    path = main_path + 'year={}/month={}/*.parquet'.format(year, month)
+    df = dd.read_parquet(path, storage_options=storage_options, engine = 'pyarrow')
+    return df
+
+def handle_time(df, utc_region, parquet = False):
+    '''Handle time column so that it is in the correct utc and calculate useful time attributes'''
+    if parquet:
+        df['ts'] = df.ts.dt.tz_localize('UTC')
+    else:
+        df['ts'] = pd.to_datetime(df['ts'], utc=True)
+    df['local_time'] = df['ts'].dt.tz_convert(utc_region)
+    time_attributes(df)
+
+def time_attributes(df):
+    '''Calculate year, month, date and hour for each jam'''
+    df['year'] = df['local_time'].dt.year
+    df['month'] = df['local_time'].dt.month
+    df['date'] = df['local_time'].dt.date
+    df['hour'] = df['local_time'].dt.hour
 
 def tci_by_period_geography(ddf, period, geography, agg_column):
     '''Returns Traffic Congestion Index'''
@@ -21,43 +41,32 @@ def mean_tci_geog(ddf, date, geog, agg_column, working_days):
     idxs = pd.MultiIndex.from_tuples(list(itertools.product(working_days, geogs)))
     daily_tci = daily_tci.reindex(idxs, fill_value = 0)
     daily_tci.reset_index(inplace = True)
-    daily_tci.rename(columns = {'level_0': 'date', 'level_1': 'city'}, inplace = True)
+    daily_tci.rename(columns = {'level_0': 'date', 'level_1': geog}, inplace = True)
     return daily_tci.groupby(geog)['tci'].mean()
-
-def time_attributes(data, utc_loc):
-    '''Converts time to datetime
-
-        Args:
-            utc_loc (pytz.BaseTzInfo): The local timezone region'''
-    data['date_utc'] = data.ts.dt.tz_localize('UTC')
-    data['date_utc'] = data.date_utc.dt.tz_convert(utc_loc)
-    data['year'] = data['date_utc'].dt.year
-    data['month'] = data['date_utc'].dt.month
-    data['date'] = data['date_utc'].dt.date
-    # data['date'] = data['date_utc'].dt.normalize()
 
 def create_gdf(data, epsg, col):
     geometry = data[col].apply(wkt.loads)
     data_geo = gpd.GeoDataFrame(data, crs="EPSG:{}".format(epsg), geometry=geometry)
     return data_geo
 
-def get_summary_statistics_street(df, street_name, year, working_days):
-    street = df[df['street'].str.contains(street_name)]
-    street = street[street['date'].isin(working_days)].copy()
-    col_name = street_name + str(year)
-    table = pd.DataFrame(index = ['number_of_jams', 'total_jam_length',
-                           'total_jam_length_level_1', 'total_jam_length_level_2',
-                           'total_jam_length_level_3', 'total_jam_length_level_4',
-                                                  ],
-                     columns = [col_name])
-    table.loc['number_of_jams', col_name] = len(street.uuid.unique())
-    table.loc['total_jam_length', col_name] = street.length.sum()/1000
-    for level in range(1, 5):
-        table.loc['total_jam_length_level_{}'.format(level), col_name] = (street[street['level']==level].length.sum())/1000
+def get_summary_statistics_street(df, street_names, year, working_days):
+    streets = df[df['street'].isin(street_names)].copy()
+    table = (streets.groupby('street')['uuid']
+             .nunique()
+             .to_frame('number_of_jams')
+             .compute())
+    table['total_jam_length'] = (streets.groupby('street')['length']
+                                 .sum()
+                                 .compute()) / 1000
 
-    daily_tci = tci_by_period_geography(street, 'date', 'street', 'lenght')
-    table.loc['tci', col_name] = daily_tci.reset_index(level = 1).reindex(working_days)['tci'].mean()
-    return table
+    by_levels = (streets.groupby(['street', 'level'])[['length']]
+                 .sum()
+                 .compute()).unstack(level=1)
+
+    for level in range(1, 5):
+        table['total_jam_length_level_{}'.format(level)] = by_levels[('length', level)]
+    table['tci'] = mean_tci_geog(streets, 'date', 'street', 'length', working_days)
+    return table.add_suffix(year)
 
 
 def get_summary_statistics_city(ddf, year, working_days):
@@ -78,13 +87,6 @@ def get_summary_statistics_city(ddf, year, working_days):
 
     return table.add_suffix(year)
 
-def convert_to_local_time(df, utc_region):
-    '''Convert date stored in string to datetime type
-        Args:
-            utc_loc (pytz.BaseTzInfo): The local timezone region'''
-    df['ts'] = pd.to_datetime(df['ts'], utc = True)
-    df['local_time'] = df['ts'].dt.tz_convert(utc_region)
-    df['hour'] = df['local_time'].dt.hour
 
 def line_to_segments(x):
     '''Break linestrings into individual segments'''
@@ -103,3 +105,13 @@ def get_jam_count_per_segment(df):
     segment_count['geometry'] = segment_count['segments'].apply(wkt.loads)
     segment_count_gdf = gpd.GeoDataFrame(segment_count, crs='epsg:4326', geometry=segment_count['geometry'])
     return segment_count_gdf
+
+def remove_last_comma(name):
+    if name[-2:] == ', ':
+        return name[:-2]
+    else:
+        return name
+def harmonize_data(table):
+    table.reset_index(inplace=True)
+    table['city'] = table['city'].apply(lambda x: remove_last_comma(x))
+    table.set_index('city', inplace=True)
